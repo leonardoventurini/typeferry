a4d3d6a682de9cfd276369916c74fb9bbea569be
Leonardo Venturini <leovenbag@gmail.com>
2026-04-21 21:01:28 -0400

    build: enable lint and dependency scanning

---
+++ bifrost-ts/eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js'
+import security from 'eslint-plugin-security'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['coverage/**', 'dist/**', 'node_modules/**'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'off',
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2022,
+        Bun: 'readonly',
+      },
+    },
+    plugins: {
+      security,
+    },
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      'no-undef': 'off',
+      'no-constant-condition': 'off',
+      'no-useless-assignment': 'off',
+      'prefer-rest-params': 'off',
+      'preserve-caught-error': 'off',
+      'require-yield': 'off',
+      'security/detect-non-literal-regexp': 'off',
+    },
+  },
+)
